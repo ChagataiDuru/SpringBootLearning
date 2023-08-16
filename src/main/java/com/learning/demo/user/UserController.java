dd6afaeea5c723987c370b21e47cea51b9895970
Çağatay <durucagatay01@gmail.com>
2023-08-16 14:30:29 +0300

    Implementing REST controllers

--- src/main/java/com/learning/demo/user/UserController.java
+++ src/main/java/com/learning/demo/user/UserController.java
@@ -1,4 +1,44 @@
 package com.learning.demo.user;
 
+import com.learning.demo.user.entity.User;
+import org.springframework.data.domain.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("users")
 public class UserController {
+
+    private final UserRepository repository;
+    public UserController(UserRepository repository) {
+        this.repository = repository;
+    }
+    @GetMapping
+    public Stream<User> findAll(@RequestParam(value = "page", defaultValue = "0") int page) {
+        int SIZE = 10;
+        Page<User> user = this.repository.findAll(PageRequest.of(page, SIZE, Sort.by("first_name", "id")));
+        return user.get();
+    }
+    @GetMapping("{id}")
+    public User findById(@PathVariable int id) {
+        return this.repository.findById(id); //.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+    @PostMapping
+    public User insert(@RequestBody User user) {
+        return this.repository.save(user);
+    }
+    @PutMapping("{id}")
+    public User update(@PathVariable int id, @RequestBody User user) {
+        User userDatabase = this.repository.findById(id);
+                //.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return this.repository.update(user);
+    }
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable int id) {
+        this.repository.deleteById(id);
+    }
+
 }
--- src/main/java/com/learning/demo/user/UserRepository.java
+++ src/main/java/com/learning/demo/user/UserRepository.java
@@ -1,4 +1,54 @@
 package com.learning.demo.user;
 
-public class UserRepository {
+import com.learning.demo.user.entity.User;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class UserRepository implements UserDAO{
+    private final EntityManager entityManager;
+
+    @Autowired
+    public UserRepository(EntityManager theEntityManager){
+        entityManager = theEntityManager;
+    }
+
+    @Override
+    @Transactional
+    public User save(User student){
+        entityManager.persist(student);
+        return student;
+    }
+
+    @Override
+    public User findById(int id) {
+        return entityManager.find(User.class, id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(int id) {
+        User student = entityManager.find(User.class, id);
+        if (student != null) {
+            entityManager.remove(student);
+        }
+    }
+
+    @Override
+    public Page<User> findAll(PageRequest of) {
+        return entityManager.createQuery("SELECT u FROM User u", User.class)
+                .setFirstResult(of.getOffset())
+                .setMaxResults(of.getPageSize())
+                .getResultList();
+    }
+
+    @Override
+    @Transactional
+    public User update(User user) {
+
+    }
 }
