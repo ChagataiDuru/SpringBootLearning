408a4e02cf72e0b3b34205a4c607de26558a31c4
Çağatay <durucagatay01@gmail.com>
2023-08-17 15:07:10 +0300

    Implemented Controller for Course and Course Repository

--- src/main/java/com/learning/demo/academic/AcademicController.java
+++ src/main/java/com/learning/demo/academic/AcademicController.java
@@ -1,5 +1,36 @@
 package com.learning.demo.academic;
 
 
+import com.learning.demo.academic.entity.Course;
+import com.learning.demo.academic.repository.CourseRepository;
+import com.learning.demo.user.UserDAO;
+import com.learning.demo.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping(name = "courses")
 public class AcademicController {
+
+    private final CourseRepository courseRepository;
+
+    public AcademicController(CourseRepository courseRepository){this.courseRepository = courseRepository;}
+
+    @GetMapping
+    public List<Course> findAllCourses(@RequestParam(value = "page", defaultValue = "0") int page) {
+        int SIZE = 10;
+        return this.courseRepository.findAll(PageRequest.of(page, SIZE, Sort.by("first_name", "id")));
+    }
+
+    
+
 }
--- src/main/java/com/learning/demo/academic/repository/CourseRepository.java
+++ src/main/java/com/learning/demo/academic/repository/CourseRepository.java
@@ -2,21 +2,32 @@ package com.learning.demo.academic.repository;
 
 import com.learning.demo.academic.dao.CourseDAO;
 import com.learning.demo.academic.entity.Course;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public class CourseRepository implements CourseDAO {
+
+    private final EntityManager entityManager;
+
+    @Autowired
+    public CourseRepository(EntityManager entityManager){
+        this.entityManager = entityManager;
+    }
+
     @Override
     @Transactional
     public Course save(Course course) {
-        return null;
+        this.entityManager.persist(course);
+        return course;
     }
 
     @Override
     public Course findById(long id) {
-        return null;
+        return this.entityManager.find(Course.class,id);
     }
 
     @Override
